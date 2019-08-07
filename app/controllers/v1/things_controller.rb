c08f0a9f7199ee9d55a8013655efb5b510581726
Giuliana Ferraro <giulianaferrarocamus@gmail.com>
2019-08-07 18:13:28 +0200

    set up react component

---
+++ app/controllers/v1/things_controller.rb
@@ -0,0 +1,11 @@
+class V1::ThingsController < ApplicationController
+  def index
+    render json: { :things => [
+      {
+        :name => 'some-thing',
+        :guid => 'some-thing-guid'
+      }
+    ]    
+  }.to_json
+  end
+end
