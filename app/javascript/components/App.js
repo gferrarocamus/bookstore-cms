d17618f2e3a741df99cde37f3569e3b777767c57
Zeha Irawan <jangkaringbumi@gmail.com>
2019-08-08 16:56:09 +0700

    setup thunk

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import {Provider} from 'react-redux'
 import PropTypes from 'prop-types';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HelloWorld from './HelloWorld';
+
+import configureStore from '../configureStore'
+
+const store = configureStore()
+
 class App extends React.Component {
   render() {
     return (
+      <Provider store={store}>
       <BrowserRouter>
         <Switch>
           <Route exact path="/" render={() => 'Home!'} />
           <Route path="/" render={() => <HelloWorld greeting="Friend" />} />
         </Switch>
       </BrowserRouter>
+      </Provider>
     );
   }
 }
